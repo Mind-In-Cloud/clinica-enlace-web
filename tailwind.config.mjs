c549249060d418680a97131b5d543132144289d6
MindInSky <kevin.a.muniz@gmail.com>
2024-06-06 19:26:55 -0600

    CE-30: Using classy for class management

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme'
-// import '@fontsource/poppins'
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
