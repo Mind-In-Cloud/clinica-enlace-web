d5dba3129818029560a671afab56a31225456d21
MindInSky <kevin.a.muniz@gmail.com>
2024-06-11 05:13:04 -0600

    CE-42:Menus

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,3 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
+document.querySelector('.k-hamburger').addEventListener('click', () => {
   document.querySelector('.nav-links').classList.toggle('expanded');
 });
