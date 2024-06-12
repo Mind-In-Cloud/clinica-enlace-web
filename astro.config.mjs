a7b891c6447991b7713359023ece72d174c25ef5
MindInSky <kevin.a.muniz@gmail.com>
2024-06-12 00:50:56 -0600

    CE-48 dist issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'clinicaenlace.mx',
+  site: 'https://clinicaenlace.mx',
   integrations: [preact(), tailwind(), sitemap()]
 });
