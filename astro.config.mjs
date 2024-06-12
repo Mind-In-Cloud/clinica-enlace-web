87a290cb6606f8e435036dbf4fab494beef6a511
MindInSky <kevin.a.muniz@gmail.com>
2024-06-12 01:41:40 -0600

    CE-48 notes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  // todo: add your public domain
   site: 'https://clinicaenlace.mx',
   integrations: [preact(), tailwind(), sitemap()]
 });
