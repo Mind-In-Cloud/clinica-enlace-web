f457672b2c4296e6b3a2db572c05a1e5c53b4087
MindInSky <kevin.a.muniz@gmail.com>
2024-06-13 02:35:51 -0600

    CE-49 issue seems to be on DoctorCard line 31-32

---
+++ src/utils/ImageDynamic.ts
@@ -0,0 +1,13 @@
+import type { ImageMetadata } from 'astro';
+
+interface Props {
+    imagePath: string;
+}
+
+export const loadImage = async (imagePath: string) => {
+    const images = import.meta.glob<{ default: ImageMetadata }>('/src/assets/images/*.{jpeg,jpg,png,gif}');
+    if (!images[imagePath]) throw new Error(`"${imagePath}" does not exist in glob: "src/assets/images/*.{jpeg,jpg,png,gif}"`);
+
+    const { default : imageInfo } = await images[imagePath]();
+    return imageInfo ;
+};
