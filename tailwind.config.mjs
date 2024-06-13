61ce04cce7b112fcd189b2299fddbc0d44176896
MindInSky <kevin.a.muniz@gmail.com>
2024-06-12 19:36:24 -0600

    CE-49 Media panel needs image-icon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,30 @@
+
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
 	theme: {
+		colors: {
+			green: '#4ED999',
+			blue: '#59CCD9',
+			red : '#D97364',
+			yellow: '#D9AC4E',
+			white: '#F8FAFC',
+		},
 		extend: {
 			fontFamily: {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans],
       },
 			colors: {
-				blue: '#59CCD9',
 				'blue-soft': '#B4E8EE',
 				cyan: '#C9EEF2',
 				'cyan-soft': '#E6F7F9',
-				green: '#4ED999',
 				'green-soft': '#AFEED1',
 				metal: '#7191AD',
 				'metal-soft': '#BFCDDA',
-				red : '#D97364',
 				'red-soft': '#EEC0B9',
-				yellow: '#D9AC4E',
 				'yellow-soft': '#EED9AF',
-				white: '#F8FAFC',
 				gray: '#6A8FA6',
 				neutral : {
 					900: '#070A0D',
