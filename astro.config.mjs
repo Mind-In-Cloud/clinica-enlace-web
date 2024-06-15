f62091f6b7b2bc070bfda9700c112552de3ed323
MindInSky <kevin.a.muniz@gmail.com>
2024-06-15 03:00:31 -0600

    CE-55 : Removed trailling slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  trailingSlash: 'never',
   // todo: add your public domain
   site: 'https://clinicaenlace.mx',
   integrations: [preact(), tailwind(), sitemap()]
