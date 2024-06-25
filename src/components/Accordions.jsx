ce406f48f03fe54b52f95c915d9dd3f11bfbb306
MindInSky <kevin.a.muniz@gmail.com>
2024-06-24 23:37:43 -0600

    CE-84: Visitantes page done

--- src/components/Accordions.jsx
+++ src/components/Accordions.jsx
@@ -2,19 +2,33 @@
 import Accordion from "@elements/Accordion";
 
 import hash from "@utils/hash";
+import classy from "@utils/classy";
 
 
 const Accordions = ( props ) => {
 
-  const { list } = props;
+  const {
+    list,
+    title = '' ,
+    color = ''
+  } = props;
+
+  const titleClasses = classy([
+    `header-s`,
+    `mb-8`,
+    `text-${color}`,
+  ])
 
   return (
     <div className="k-accordions k-section">
+      { title && <h3 {...titleClasses}>{title}</h3> }
       { list.map((item, index) => (
         <Accordion
           key={hash(item,index)}
+          color={color}
           title={item.title}
           content={item.content}
+          final = { index === list.length - 1}
         />
       ))}
     </div>
--- src/elements/Accordion.jsx
+++ src/elements/Accordion.jsx
@@ -3,15 +3,23 @@ import { useState } from 'preact/hooks';
 import classy from '@utils/classy';
 
 
-const Accordion = ({ title, content, startsOpen = false }) => {
+const Accordion = ( props ) => {
+
+  const {
+    title,
+    content,
+    startsOpen = false,
+    color = '',
+    final = false,
+  } = props
 
   const [ open, setOpen ] = useState(startsOpen)
 
   const accordionClasses = classy([
     'k-accordion mb-2',
-    'rounded',
-    'shadow-md',
+    color === 'green' ? 'border-t border-[#00000054]' : 'rounded shadow-md',
     open && 'k-open',
+    color == 'green' && final && 'border-b border-[#00000054]',
   ])
 
   const buttonClasses = classy([
@@ -20,7 +28,7 @@ const Accordion = ({ title, content, startsOpen = false }) => {
     'flex',
     'justify-between',
     'items-center',
-    'p-4 pl-8',
+    color === 'green' ? 'py-4' : 'p-4 pl-8',
   ])
 
   const titleClasses = classy([
@@ -28,7 +36,7 @@ const Accordion = ({ title, content, startsOpen = false }) => {
   ])
 
   const contentClasses = classy([
-    'pb-4 pr-4 pl-8',
+    color === 'green' ? 'pb-4' : 'pb-4 pr-4 pl-8',
     'whitespace-pre-line',
     'typo-body-m lg:typo-body-xl text-neutral-500 antialiased',
     open ? 'k-open opacity-1' : 'k-closed opacity-0 hidden',
@@ -47,15 +55,29 @@ const Accordion = ({ title, content, startsOpen = false }) => {
         {
           open?
           <div>
-            <svg width="22" height="8" viewBox="0 0 22 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M21.6941 0.743999V7.56H0.574125V0.743999H21.6941Z" fill="#D9AC4E"/>
-            </svg>
+            {
+              color === 'green' ?
+              <svg width="26" height="9" viewBox="0 0 26 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path opacity="0.8" d="M25.552 0.867999V8.82H0.912V0.867999H25.552Z" fill="#42B982"/>
+              </svg>
+              :
+              <svg width="22" height="8" viewBox="0 0 22 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21.6941 0.743999V7.56H0.574125V0.743999H21.6941Z" fill="#D9AC4E"/>
+              </svg>
+            }
           </div>
           :
           <div>
-            <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M25.2417 16.896H16.8897V25.44H9.11369V16.896H0.761688V9.552H9.11369V0.959999H16.8897V9.552H25.2417V16.896Z" fill="#D9AC4E"/>
-            </svg>
+            {
+              color === 'green' ?
+              <svg width="29" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path opacity="0.8" d="M28.864 19.212H19.12V29.18H10.048V19.212H0.304V10.644H10.048V0.619999H19.12V10.644H28.864V19.212Z" fill="#42B982"/>
+              </svg>
+              :
+              <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M25.2417 16.896H16.8897V25.44H9.11369V16.896H0.761688V9.552H9.11369V0.959999H16.8897V9.552H25.2417V16.896Z" fill="#D9AC4E"/>
+              </svg>
+            }
           </div>
         }
       </button>
