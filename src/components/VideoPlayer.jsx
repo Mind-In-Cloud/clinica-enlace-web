7e11def89b9c8de2b657a3697968bf155de2f625
MindInSky <kevin.a.muniz@gmail.com>
2024-06-26 21:55:10 -0600

    CE-90: Video component done

---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'preact/hooks';
+import videoImage from '@images/video.png'
+
+const VideoPlayer = () => {
+
+  const [isImageVisible, setIsImageVisible] = useState(true);
+
+  const handleClick = () => {
+    if (isImageVisible) {
+      setIsImageVisible(false);
+    }
+  };
+
+
+  return (
+    <div class='w-full aspect-video'>
+      {isImageVisible ?
+        <img
+          {...videoImage}
+          src={`/.netlify/images?url=${videoImage.src}`}
+          class="aspect-video w-full object-cover"
+          onClick={handleClick}
+          alt={`Imagen para activar el video`}
+        />
+      :
+        <iframe
+          class="aspect-video w-full"
+          src="https://www.youtube-nocookie.com/embed/5OZAqjIbP_U?si=5Tuki9cImzOtchDx&amp;controls=0"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      }
+    </div>
+
+  );
+};
+
+export default VideoPlayer;
